571d7f7a15a7a87ac417cf52b5bf91c822b0b7b9
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-02-26 19:36:57 -0700

    Complete GET slips route test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,6 +3,10 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
@@ -16,8 +20,50 @@ describe('Client Routes', () => {
       done();
     });
   });
+  it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+    .get('/sad')
+    .end((err, response) => {
+      response.should.have.status(404);
+      done();
+    });
+  });
 });
 
 describe('API Routes', () => {
+  before((done) => {
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  describe('GET /api/v1/slips', () => {
+ it('should return all of the slips of advice', done => {
+    chai.request(server)
+      .get('/api/v1/slips')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(3);
+        console.log(response.body)
+        response.body[0].should.have.property('advice');
+        response.body[0].should.have.property('id');
+        response.body[0].should.have.property('created_at');
+        response.body[0].should.have.property('updated_at');
+        done();
+      });
+    });
+  });
 
 });
