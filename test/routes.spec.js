9f8379f99a6e9ca1d073da1f88e478a9f2530344
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-02-26 19:25:35 -0700

    Write initial test for root route

---
+++ test/routes.spec.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  it('should return the homepage with text', done => {
+    chai.request(server)
+    .get('/')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.should.equal('To contribute advice to this API visit https://mnhollandplum.github.io/magic_8_ball/');
+      done();
+    });
+  });
+});
+
+describe('API Routes', () => {
+
+});
